0e1621b9c27b61cc394a8a9c6fa345a4767f9e52
Yuyang Liu <lhtlyy@gmail.com>
2015-04-08 12:16:43 +0800

    Revert "	modified:   bootpack.c"
    This reverts commit e2fcea9ec3963df912643fe79cea992ccbb0aca3.

--- bootpack.h
+++ bootpack.h
@@ -61,15 +61,9 @@ struct FIFO8 mouseinfo;   // FIFO8结构体
 #define KEYCMD_SENDTO_MOUSE 0xd4
 #define MOUSECMD_ENABLE 0xf4
 
-struct MOUSE_DEC {
-    unsigned char buf[3], phase;
-    int x, y, btn;
-};
-
 void wait_KBC_sendready(void);
 void init_keyboard(void);
-void enable_mouse(struct MOUSE_DEC *mdec);
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+void enable_mouse(void);
 
 // 启动信息
 struct BOOTINFO {
