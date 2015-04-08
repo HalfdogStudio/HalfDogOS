e2fcea9ec3963df912643fe79cea992ccbb0aca3
Yuyang Liu <lhtlyy@gmail.com>
2015-04-08 11:10:20 +0800

    modified:   bootpack.c 	modified:   bootpack.h

--- bootpack.c
+++ bootpack.c
@@ -9,6 +9,8 @@ void HalfDogMain(void){
     // 键盘鼠标中断变量
     int i; //存放键盘数据
     char s[40], keybuf[32], mousebuf[128];
+    struct MOUSE_DEC mdec;
+    int mx, my;     //鼠标
 
     init_gdtidt();
     init_pic();
@@ -19,7 +21,7 @@ void HalfDogMain(void){
     fifo8_init(&keyinfo, 32, keybuf);
     fifo8_init(&mouseinfo, 128, mousebuf);
     init_keyboard();        // 鼠标和键盘控制电路是一个
-    enable_mouse();
+    enable_mouse(&mdec);
 
     init_palette(); // 调色板
 
@@ -30,7 +32,7 @@ void HalfDogMain(void){
     //putfont8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_WHITE, "HalfDog OS");
 
     init_mouse_cursor8(mcursor, COL8_DARK_CYAN);
-    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, 140, 80, mcursor, 16);
+    //putblock8_8(binfo->vram, binfo->scrnx, 16, 16, 140, 80, mcursor, 16);
 
     for(;;){
         io_cli();                   //禁止中断
@@ -45,8 +47,37 @@ void HalfDogMain(void){
             } else if (fifo8_status(&mouseinfo) != 0){
                 i = fifo8_get(&mouseinfo);
                 io_sti();               //恢复中断,不因为图像处理阻塞
-                boxfill8(binfo->vram, binfo->scrnx, COL8_DARK_CYAN, 0, 17, 32 * 8 - 1, 32);
-                putfont8_hex(binfo->vram, binfo->scrnx, 3, 19, COL8_WHITE, (unsigned char *)&i);
+                if (mouse_decode(&mdec, i) != 0) {
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_DARK_CYAN, 0, 17, binfo->scrnx-1, 32);
+                    char mouse_status[] = "lcr";
+                    if (mdec.btn&0x1 != 0){
+                        mouse_status[0] = 'L';
+                    }
+                    if (mdec.btn>>1&0x1 != 0){
+                        mouse_status[2] = 'R';
+                    }
+                    if (mdec.btn>>2&0x1 != 0){
+                        mouse_status[1] = 'C';
+                    }
+                    putfont8_asc(binfo->vram, binfo->scrnx, 3, 19, COL8_WHITE, mouse_status);
+                    // 隐藏鼠标
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_DARK_CYAN, mx, my, mx+15, my+15);
+                    mx += mdec.x;
+                    my += mdec.y;
+                    if (mx < 0) {
+                        mx = 0;
+                    }
+                    if (mx > binfo->scrnx - 16) {
+                        mx = binfo->scrnx - 16;
+                    }
+                    if (my < 0) {
+                        my = 0;
+                    }
+                    if (my > binfo->scrny - 16) {
+                        my = binfo->scrny - 16;
+                    }
+                    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+                }
             }
         }
     }
@@ -70,12 +101,51 @@ void init_keyboard(void){
     return;
 }
 
-void enable_mouse(void){
+void enable_mouse(struct MOUSE_DEC *mdec){
     // 激活鼠标
     wait_KBC_sendready();
     io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
     wait_KBC_sendready();
     io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+    mdec->phase = 0;    //等待0xfa状态
     return; //如果顺利键盘控制返回ACK(0xfa)
 }
 
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat){
+    if (mdec->phase == 0){
+        if(dat == 0xfa){
+            mdec->phase = 1;
+        }
+        return 0;
+    } else if (mdec->phase == 1){
+        //等待鼠标的第一字节
+        if ((dat & 0xc8) == 0x08) {
+            // 第一字节高四位在0-3范围内,低四位在8-F范围
+            // 防止鼠标接触不良
+            mdec->buf[0] = dat;
+            mdec->phase = 2;
+        }
+        return 0;
+    } else if (mdec->phase == 2){
+        //等待鼠标的第二字节
+        mdec->buf[1] = dat;
+        mdec->phase = 3;
+        return 0;
+    } else if(mdec->phase == 3){
+        mdec->buf[2] = dat;
+        mdec->phase = 1;
+        // 鼠标的3个字节都凑齐了
+        mdec->btn = mdec->buf[0] & 0x07; //第一个字符的低三位,鼠标状态(中键|右键|左键)
+        mdec->x = mdec->buf[1];  //第二个字节
+        mdec->y = mdec->buf[2]; //第三个字节
+        if ((mdec->buf[0] & 0x10) != 0) {   //第一个字节中(第5位)对鼠标x移动有用的位
+            mdec->x |= 0xffffff00;          //高24位全置1
+        }
+        if ((mdec->buf[0] & 0x20) != 0) {   //第一个字节中(第6位)对鼠标x移动有用的位
+            mdec->y |= 0xffffff00;          //高24位全置1
+        }
+        mdec->y = -mdec->y;                 //鼠标y坐标和画面相反
+        return 1;
+    }
+    return -1;
+}
